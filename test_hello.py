4a915405a8bd5838181b134df600129a87048a73
Jinesh Shinde <samratjinesh619@gmail.com>
2020-11-29 20:09:10 +0530

    test cases

---
+++ test_hello.py
@@ -0,0 +1,17 @@
+import unittest
+from hello_world import Addition
+
+
+class TestAddition(unittest.TestCase):
+
+    def setup(self):
+        self.a = 20
+        self.b = 30
+
+    def test_addition(self):
+        obj = Addition(20, 30)
+        res = obj.addition()
+        assert(res, 50)
+
+if __name__ == '__main__':
+    unittest.main()
